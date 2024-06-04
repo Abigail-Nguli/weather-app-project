6172a116ef3e3bff8cabf4b825ea441e46ad7ff8
Abigail-Nguli <143516715+Abigail-Nguli@users.noreply.github.com>
2024-06-05 00:10:39 +0300

    Updated city search and temperature

--- src/index.js
+++ src/index.js
@@ -1,9 +1,30 @@
+function updateWeather(response) {
+  let temperature = document.querySelector("#temp");
+
+  let heading = document.querySelector("#city");
+  cityHeading = response.data.city
+  heading.innerHTML = `${cityHeading} |`;
+
+  let country = document.querySelector("#country");
+  country.innerHTML = response.data.country;
+
+  temp = Math.round(response.data.temperature.current)
+
+  temperature.innerHTML = `${temp} °C`;
+}
+
+function searchTemp(cityValue) {
+  let apiKey = "fa483db98dc0o4b7fc5fdbea841a31ta";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${cityValue}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(updateWeather);
+}
+
 function searchCity(event) {
   event.preventDefault();
   let city = document.querySelector("#searchBar");
 
-  let heading = document.querySelector("h1");
-  heading.innerHTML = city.value;
+  searchTemp(city.value);
 }
 
 let searchButton = document.querySelector("#searchBtn");
