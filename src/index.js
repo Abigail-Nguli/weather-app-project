8767a1fed0e0282bb8132fae68908f228b00e0e9
Abigail-Nguli <143516715+Abigail-Nguli@users.noreply.github.com>
2024-06-06 12:28:06 +0300

    Updated the forecast

--- src/index.js
+++ src/index.js
@@ -69,18 +69,18 @@ function searchTemp(cityValue) {
   axios.get(apiUrl).then(updateWeather);
 }
 
-function searchCity(event) {
-  event.preventDefault();
-  let cityElement = document.querySelector("#searchBar");
-
-  searchTemp(cityElement.value);
-}
-
 function getForecast(city) {
   let apiKey = "fa483db98dc0o4b7fc5fdbea841a31ta";
   let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}`;
 
   axios.get(apiUrl).then(showForecast);
+}
+
+function searchCity(event) {
+  event.preventDefault();
+  let cityElement = document.querySelector("#searchBar");
+
+  searchTemp(cityElement.value);
 }
 
 function showForecast(response) {
@@ -109,6 +109,7 @@ function showForecast(response) {
             `;
     }
   });
+  
 }
 
 let city = "Nairobi";
@@ -116,122 +117,3 @@ searchTemp(city);
 
 let searchButton = document.querySelector("#searchBtn");
 searchButton.addEventListener("click", searchCity);
-
-/*function changeTime(timeNow) {
-  let currentTime = new Date();
-
-  let weekDay = document.querySelector("#Day");
-  let day = currentTime.getDay();
-
-  let days = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-  ];
-  weekDay.innerHTML = days[day];
-
-  let hours = currentTime.getHours();
-  let minutes = currentTime.getMinutes();
-
-  let time = document.querySelector("#tempTime");
-
-  if (minutes < 10) {
-    time.innerHTML = `${hours}:0${minutes}`;
-  } else {
-    time.innerHTML = `${hours}:${minutes}`;
-  }
-}
-
-function updateDay(unixTime) {
-  let day = new Date(unixTime * 1000);
-  let days = ["Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"];
-
-  return days[day.getDay()];
-}
-
-function updateWeather(response) {
-  let temperature = document.querySelector("#temp");
-
-  let heading = document.querySelector("#city");
-  cityHeading = response.data.city;
-  heading.innerHTML = `${cityHeading} |`;
-
-  let country = document.querySelector("#country");
-  country.innerHTML = response.data.country;
-
-  let description = document.querySelector("#description");
-  description.innerHTML = response.data.condition.description;
-
-  let speed = document.querySelector("#speed");
-  speed.innerHTML = response.data.wind.speed;
-
-  temp = Math.round(response.data.temperature.current);
-
-  temperature.innerHTML = `${temp} °C`;
-
-  let timeElement = document.querySelector("#tempTime");
-  changeTime(timeElement);
-
-  let icon = document.querySelector(".icon");
-  icon.innerHTML = `<img src="${response.data.condition.icon_url}" id="icon">`;
-
-  getForecast(response.data.city);
-}
-
-function searchTemp(cityValue) {
-  let apiKey = "fa483db98dc0o4b7fc5fdbea841a31ta";
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${cityValue}&key=${apiKey}`;
-
-  axios.get(apiUrl).then(updateWeather);
-}
-
-function searchCity(event) {
-  event.preventDefault();
-  let city = document.querySelector("#searchBar");
-
-  searchTemp(city.value);
-}
-
-function getForecast(city) {
-  let apiKey = "fa483db98dc0o4b7fc5fdbea841a31ta";
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
-
-  axios.get(apiUrl).then(showForecast);
-}
-
-function showForecast(response) {
-  let forecast = document.querySelector("#forecast");
-  forecast.innerHTML = " ";
-
-  response.data.daily.forEach(function (day, i) {
-    if (i < 5) {
-      forecast.innerHTML += `
-  <ul class="weekday">
-            <li class="dayForecast>
-              <span class="day"> ${updateDay(day.time)} </span>
-              <img src="${day.condition.icon_url}" class="symbol">
-              <p>
-                <span class="predictedTemp"> ${Math.round(
-                  day.temperature.maximum
-                )} / </span>
-                  <span class="predictedTemp right">${Math.round(
-                    day.temperature.minimum
-                  )} <br /> </span>
-                <span class="description"> ${day.condition.description} </span>
-              </p>
-            </li>
-          </ul>
-  `;
-    }
-  });
-}
-
-city = "Nairobi";
-searchTemp(city);
-
-let searchButton = document.querySelector("#searchBtn");
-searchButton.addEventListener("click", searchCity);*/
